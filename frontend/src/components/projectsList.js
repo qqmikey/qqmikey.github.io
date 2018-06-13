313bc0ec3eeb83851783e7d0c56199e2cef63640
mikey <qqmikey@icloud.com>
2018-06-13 23:12:32 +0500

    projects page with sort

--- frontend/src/components/projectsList.js
+++ frontend/src/components/projectsList.js
@@ -13,7 +13,11 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import red from '@material-ui/core/colors/red';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
 
 import strings from "../strings";
 
@@ -41,11 +45,20 @@ const styles = theme => ({
     expandOpen: {
         transform: 'rotate(180deg)',
     },
-    avatar: {
-        backgroundColor: red[500],
-    },
     chip: {
         margin: theme.spacing.unit,
+    },
+    gridList: {
+        flexWrap: 'nowrap',
+        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+        transform: 'translateZ(0)',
+    },
+    title: {
+        color: theme.palette.primary.light,
+    },
+    titleBar: {
+        background:
+            'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
     },
 });
 
@@ -79,91 +92,64 @@ class Projects extends React.Component {
                 <h2>{strings[this.props.lang].titles.projects}</h2>
                 <div>
                     {
-                        this.state.projects.map((item, i) => {
+                        this.state.projects.sort((a, b) => {return b.position - a.position}).map((item, i) => {
                             return (
                                 <div key={i}>
                                     <Card className={classes.card}>
                                         <CardHeader
                                             avatar={
-                                                <Avatar aria-label="Recipe" className={classes.avatar}>
-                                                    R
-                                                </Avatar>
+                                                <Avatar
+                                                    src={item.logo || 'http://tinygraphs.com/labs/isogrids/hexa16/' + item.title + '?theme=sugarsweets&numcolors=2&size=48&fmt=svg'}/>
                                             }
-                                            action={
-                                                <IconButton>
-                                                    <MoreVertIcon/>
-                                                </IconButton>
-                                            }
-                                            title={item.desc}
-                                            subheader="September 14, 2016"
+                                            action={item.expandable ?
+                                                <IconButton
+                                                    className={classnames(classes.expand, {
+                                                        [classes.expandOpen]: item.expanded,
+                                                    })}
+                                                    onClick={this.handleExpandClick.bind(this, i)}
+                                                    aria-expanded={this.state.expanded}
+                                                >
+                                                    <ExpandMoreIcon/>
+                                                </IconButton> : null}
+                                            title={item.title}
+                                            subheader={item.date}
                                         />
                                         <CardContent>
                                             <Typography component="p">
-                                                This impressive paella is a perfect party dish and a fun meal to cook
-                                                together with
-                                                your guests. Add 1 cup of frozen peas along with the mussels, if you
-                                                like.
+                                                {item.short_desc}
                                             </Typography>
                                         </CardContent>
                                         <CardActions className={classes.actions} disableActionSpacing>
                                             {
-                                                item.chips !== undefined && item.chips.map((chip, i) => {
-                                                   return (
-                                                       <Chip key={i} label={chip} className={classes.chip} />
-                                                   )
+                                                item.chips && item.chips.map((chip, i) => {
+                                                    return (
+                                                        <Chip key={i} label={chip} className={classes.chip}/>
+                                                    )
                                                 })
                                             }
-                                            <IconButton
-                                                className={classnames(classes.expand, {
-                                                    [classes.expandOpen]: item.expanded,
-                                                })}
-                                                onClick={this.handleExpandClick.bind(this, i)}
-                                                aria-expanded={this.state.expanded}
-                                                aria-label="Show more"
-                                            >
-                                                <ExpandMoreIcon/>
-                                            </IconButton>
                                         </CardActions>
                                         <Collapse in={item.expanded} timeout="auto" unmountOnExit>
                                             <CardContent>
-                                                <Typography paragraph variant="body2">
-                                                    Method:
-                                                </Typography>
+
                                                 <Typography paragraph>
-                                                    Heat 1/2 cup of the broth in a pot until simmering, add saffron and
-                                                    set aside for 10
-                                                    minutes.
+                                                    {item.desc}
                                                 </Typography>
-                                                <Typography paragraph>
-                                                    Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-                                                    over medium-high
-                                                    heat. Add chicken, shrimp and chorizo, and cook, stirring
-                                                    occasionally until lightly
-                                                    browned, 6 to 8 minutes. Transfer shrimp to a large plate and set
-                                                    aside, leaving
-                                                    chicken and chorizo in the pan. Add pimentón, bay leaves, garlic,
-                                                    tomatoes, onion,
-                                                    salt and pepper, and cook, stirring often until thickened and
-                                                    fragrant, about 10
-                                                    minutes. Add saffron broth and remaining 4 1/2 cups chicken broth;
-                                                    bring to a boil.
-                                                </Typography>
-                                                <Typography paragraph>
-                                                    Add rice and stir very gently to distribute. Top with artichokes and
-                                                    peppers, and
-                                                    cook without stirring, until most of the liquid is absorbed, 15 to
-                                                    18 minutes.
-                                                    Reduce heat to medium-low, add reserved shrimp and mussels, tucking
-                                                    them down into
-                                                    the rice, and cook again without stirring, until mussels have opened
-                                                    and rice is
-                                                    just tender, 5 to 7 minutes more. (Discard any mussels that don’t
-                                                    open.)
-                                                </Typography>
-                                                <Typography>
-                                                    Set aside off of the heat to let rest for 10 minutes, and then
-                                                    serve.
-                                                </Typography>
+
+                                                <GridList className={classes.gridList} cols={2.5}>
+                                                    {item.images && item.images.map((tile, i) => (
+                                                        <GridListTile key={i}>
+                                                            <img src={tile.img}/>
+                                                            <GridListTileBar
+                                                                title={tile.title}
+                                                                classes={{
+                                                                    root: classes.titleBar,
+                                                                    title: classes.title,
+                                                                }}
+                                                            />
+                                                        </GridListTile>
+                                                    ))}
+                                                </GridList>
+
                                             </CardContent>
                                         </Collapse>
                                     </Card>
